a67b5451cf556d5befe455a7d3e6cef51de6c40c
Senpair Alemneh Humnessa <senpair.alemneh@aastustudent.edu.et>
2024-12-02 17:47:20 +0300

    file transfer.cpp

---
+++ ETS 1271_Senpair Alemneh /file transfer.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  //declare transmission_rate,filesize,transmission_time
+
+    int transmission_rate=960 ;
+    int bytes_per_character=1;
+    long long filesize;
+    int transmission_time;
+  
+//acccept file size  
+cout<<"enter file size in bytes: "<<endl;
+cin>>filesize;
+  
+//calculate transmission_time in days,hours,minutes,seconds  
+transmission_time=(filesize)/(transmission_rate*bytes_per_character);
+
+int days=transmission_time/(24*3600);
+
+transmission_time= (transmission_time%24*3600);
+int hour=transmission_time/(3600);
+
+transmission_time= (transmission_time%3600);
+int min=transmission_time/60;
+
+int sec=(transmission_time%60);
+  
+//print transmission time
+cout<<"transmission time is: "<<days<<"days: "<<hour<<"hours: "<<min<<"minutes"<<sec<<"seconds"<<endl;
