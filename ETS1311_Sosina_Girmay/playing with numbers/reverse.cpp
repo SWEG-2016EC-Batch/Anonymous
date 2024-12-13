82840a0e4a6d8993a6aabd141510d543d532b76b
Sosina Girmay Mehari <sosina.girmay@aastustudent.edu.et>
2024-12-13 10:26:31 +0300

    Update reverse.cpp

--- ETS1311_Sosina_Girmay/playing with numbers/reverse.cpp
+++ ETS1311_Sosina_Girmay/playing with numbers/reverse.cpp
@@ -2,7 +2,7 @@
 using namespace std; 
 int main(){
     // printing the reverse of an integer.
-    int num, sum = 0, remainder, temp;
+    int num, reverse = 0, remainder, temp;
     label1:
     cout<<"Please enter a number: ";
     cin>>num;
@@ -16,16 +16,16 @@ int main(){
     temp = num;
     do{
         remainder = num % 10;
-        sum += remainder;
+        reverse += remainder;
         num /= 10;
         if(num == 0){
         	break;
 		}
 		else{
-			sum *= 10;
+			reverse *= 10;
 		}
     }while(num != 0);
-    cout<<"The rever of "<<temp<<" is "<<sum;
+    cout<<"The rever of "<<temp<<" is "<<reverse;
 
 
     return 0;
