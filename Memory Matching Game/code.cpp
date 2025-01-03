ec853d0d34c3caf9f1a1b10dd994ea3179d22c04
Senpair Alemneh Humnessa <senpair.alemneh@aastustudent.edu.et>
2025-01-03 14:17:30 +0300

    code.cpp

--- Memory Matching Game/code.cpp
+++ Memory Matching Game/code.cpp
@@ -197,56 +197,6 @@ int main()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
  revealed[row2][col2] = false;
             shuffle(column);
             losses++;
