88b8f68742522a44158becf6939a67e225c30f8d
Senpair Alemneh Humnessa <senpair.alemneh@aastustudent.edu.et>
2025-01-03 14:16:51 +0300

    code.cpp

--- Memory Matching Game/code.cpp
+++ Memory Matching Game/code.cpp
@@ -1,4 +1,4 @@
-  #include <iostream>
+#include <iostream>
 #include <cstdlib>// for rand and srand functions and also for system clear which can be used to clear the previous output
 #include <ctime> // for time function
 using namespace std;
@@ -48,7 +48,56 @@ void shuffle(int column)
       // Swap the now element with the random element
             int temp = grid[nowRow][nowColumn];
             grid[nowRow][nowColumn] = grid[randomRowIndex][randomColumnIndex];
+	    grid[randomRowIndex][randomColumnIndex] = temp;
+        }
+    }
+}
 
+int main() 
+{
+	// change the background color of the console to purple
+	system("color 5e");
+	
+	// initalize counters 
+	//declare the column for the different levels 
+    int column, counter = 0, wins = 0, losses = 0;
+
+	// the menu
+    cout << "******* MENU *******\n";
+    cout << "Please choose the level of the game.\n";
+    cout << "1. Easy \n2. Medium \n3. Hard\n";
+    cin >> column;
+
+	// if the user input invalid column, accepts the column again
+    while (column < 1 || column > 3)
+	{
+        cout << "Invalid input! Please choose the right menu.\n";
+        cin >> column;
+    }
+
+	// allow the program to adjust the level based on the user's column choice
+    switch (column)
+	{
+        case 1: 
+		column = 4;
+		break;
+        case 2: 
+		column = 6; 
+		break;
+        case 3: 
+		column = 8; 
+		break;
+    }
+
+    // Initialize grid with pairs of numbers
+    int counterValue = 0;
+    // Initialize a counter to generate the numbers to be placed in the grid.
+    for (int i = 0; i < ROWS; ++i) 
+	{
+        for (int j = 0; j < column; ++j)
+		{
+            grid[i][j] = counterValue++;
+            // Assign the current value of counterValue to the current grid cell.
 
 
 
