e0a98a0ccdad57a576c31f71b1b74d85a299be42
Senpair Alemneh Humnessa <senpair.alemneh@aastustudent.edu.et>
2025-01-03 12:56:49 +0300

    code.cpp

--- Memory Matching Game/code.cpp
+++ Memory Matching Game/code.cpp
@@ -1,54 +1,50 @@
-#include <iostream>
-#include <cstdlib>// for rand and srand functions and also for system clear which can be used to clear the previous output
-#include <ctime> // for time function
-using namespace std;
-
-// declare the necessary variables 
-const int ROWS = 4;
-const int MAX_COLUMNS = 8; // set it to the maximum nuber of columns 
-int grid[ROWS][MAX_COLUMNS]; // declare the an array for the grid 
-bool revealed[ROWS][MAX_COLUMNS] = { false }; // Initialize a boolean array to track revealed cards, defaulting all to false initially.
-
-// function to shuffle 
-void shuffle(int column) 
-{
-    srand(time(0));
-    /* srand function help us to change the seed. There is an information about seed in the documentaion.
-    - time function help generate new seed value  */
-for (int i = 0; i < ROWS * column; ++i) 
-	{
-/* - now lets change the 2d array into a 1d array.
-		   - why? because the rand function generates only one integer and we have row index and column index which means we will have to generate two numbers
-		   - to do that we will divide i by column for row index  which gives us the index 0 
-		   - for column index we will make it the remainder */
-        int nowRow = i / column;
-        int nowColumn = i % column;
-
-		
-        if (!revealed[nowRow][nowColumn])
-		 { 
-		// This condition checks if the  cell in the grid is not yet revealed.
-   		// - If the cell is not revealed (still hidden), we proceed to generate a random index for shuffling.
-    	// - This ensures that only unrevealed cells are shuffled, preserving the state of already revealed cells.
-            int randomIndex;
-            do 
-			{
-                randomIndex = rand() % (ROWS * column);
-                // Generates a random index for shuffling within the grid size.
-            } 
-			while (revealed[randomIndex / column][randomIndex % column]); 
-			
-			// now change the 1d index to 2d index randomRowIndex and randomColumnIndex
-			// how? Dividing the index by column gives the row number.
-            int randomRowIndex = randomIndex / column;
-            // remainder of the index divided by column gives the row number.
-            int randomColumnIndex = randomIndex % column;
-			
-			// Swap the now element with the random element
-            int temp = grid[nowRow][nowColumn];
-            grid[nowRow][nowColumn] = grid[randomRowIndex][randomColumnIndex];
-            grid[randomRowIndex][randomColumnIndex] = temp;
+          grid[randomRowIndex][randomColumnIndex] = temp;
         }
     }
 }
 
+int main() 
+{
+	// change the background color of the console to purple
+	system("color 5e");
+	
+	// initalize counters 
+	//declare the column for the different levels 
+    int column, counter = 0, wins = 0, losses = 0;
+
+	// the menu
+    cout << "******* MENU *******\n";
+    cout << "Please choose the level of the game.\n";
+    cout << "1. Easy \n2. Medium \n3. Hard\n";
+    cin >> column;
+
+	// if the user input invalid column, accepts the column again
+    while (column < 1 || column > 3)
+	{
+        cout << "Invalid input! Please choose the right menu.\n";
+        cin >> column;
+    }
+
+	// allow the program to adjust the level based on the user's column choice
+    switch (column)
+	{
+        case 1: 
+		column = 4;
+		break;
+        case 2: 
+		column = 6; 
+		break;
+        case 3: 
+		column = 8; 
+		break;
+    }
+
+    // Initialize grid with pairs of numbers
+    int counterValue = 0;
+    // Initialize a counter to generate the numbers to be placed in the grid.
+    for (int i = 0; i < ROWS; ++i) 
+	{
+        for (int j = 0; j < column; ++j)
+		{
+            grid[i][j] = counterValue++;
+            // Assign the current value of counterValue to the current grid cell.
